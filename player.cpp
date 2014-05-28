70e1a703258310e038926579d88975bff2137eb2
zemei <zemei88@gmail.com>
2014-05-28 10:12:54 +0200

    d

--- player.cpp
+++ player.cpp
@@ -3,7 +3,11 @@
 
 const char* Player::VERSION = "Default C++ folding player";
 
-
+struct GameState {
+    int small_blind;
+    int stack;
+    bool has_A;
+};
 
 
 //////////////////////////////////
@@ -45,7 +49,15 @@ const char* Player::VERSION = "Default C++ folding player";
 //START of Code section for Tibi
 int Player::betRequest(json::Value game_state)
 {
-    return 1000;
+    GameState gs;
+    gs.small_blind = game_state["small_blind"].ToInt();
+
+    json::Value me = game_state["players"][game_state["in_action"].ToInt()];
+    gs.stack = me["stack"];
+
+    gs.has_A = me["hand"]["hole_cards"].ToArray()[0]["rank"].ToString() == "A" || me["hand"]["hole_cards"].ToArray()[1]["rank"].ToString() == "A";
+
+    return gs.has_A ? 1000 : 0;
 }
 
 void Player::showdown(json::Value game_state)
