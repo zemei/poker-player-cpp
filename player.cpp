1e87e25835b5cc81597771cd37c2d3d955404108
zemei <zemei88@gmail.com>
2014-05-28 09:58:29 +0200

    first try

--- player.cpp
+++ player.cpp
@@ -1,12 +1,55 @@
 #include "player.h"
+//Continous delivery poker
 
 const char* Player::VERSION = "Default C++ folding player";
 
+
+
+
+//////////////////////////////////
+//START of Code section for Laja
+
+
+//END of Code section for Laja
+//////////////////////////////////
+
+
+
+
+
+
+//////////////////////////////////
+//START of Code section for Gabor
+
+
+//END of Code section for Gabor
+//////////////////////////////////
+
+
+
+
+
+
+
+//////////////////////////////////
+//START of Code section for Andris
+
+
+//END of Code section for Andris
+//////////////////////////////////
+
+
+
+
+//////////////////////////////////
+//START of Code section for Tibi
 int Player::betRequest(json::Value game_state)
 {
-    return 0;
+    return 1000;
 }
 
 void Player::showdown(json::Value game_state)
 {
 }
+//END of Code section for Tibi
+//////////////////////////////////
